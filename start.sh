e52fbc91d02d6af252456d69bf58b6ece5e4b3f9
IraqLobsterDE <79744524+IraqLobsterDE@users.noreply.github.com>
2023-11-29 22:02:23 +0100

    removed code that became unnecessary do to changes in the installation process of the server

--- start.sh
+++ start.sh
@@ -2,14 +2,6 @@
 # Exit on error
 set -e
 
-# Ensure config files exist
-for file in /default-config/*.ini; do
-    if [ ! -f /config/$(basename $file) ]; then
-        cp $file /config/$(basename $file)
-        echo 'Recreated "/config/'$(basename $file)'" from "'$file'"'
-    fi
-done
-
 # Ensure Database exists and is initialized
 export MYSQL_PWD="$MYSQL_PASSWORD"
 MYSQL_AUTHSTR=" --host $MYSQL_HOST -u $MYSQL_USERNAME "
@@ -20,16 +12,11 @@ else
     echo "Database does not exist"
     echo 'Creating database "'$MYSQL_DATABASE'" on host "'$MYSQL_HOST'"...'
     mysql $MYSQL_AUTHSTR -e "CREATE DATABASE $MYSQL_DATABASE"
-    echo 'Initializing database...'
-    for file in /db-init/*.sql; do
-        echo 'Importing "'$file'"...'
-        mysql $MYSQL_AUTHSTR $MYSQL_DATABASE < $file
-    done
     echo 'Database "'$MYSQL_DATABASE'" initialized successfully!'
 fi
 
 # Set mysql config parameters based on evironment variables
-for file in /config/*.ini; do
+for file in /config/masterconfig.ini; do
     sed -i "s/mysql_host.*/mysql_host=$MYSQL_HOST/g" $file
     sed -i "s/mysql_database.*/mysql_database=$MYSQL_DATABASE/g" $file
     sed -i "s/mysql_username.*/mysql_username=$MYSQL_USERNAME/g" $file
