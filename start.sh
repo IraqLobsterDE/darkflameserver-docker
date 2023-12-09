4a6644d26332998349d352e029433c2d57dbc536
IraqLobsterDE <79744524+IraqLobsterDE@users.noreply.github.com>
2023-12-09 01:49:19 +0100

    Added IP Variables

--- start.sh
+++ start.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 # Exit on error
-set -e
-
-# Ensure Database exists and is initialized
-export MYSQL_PWD="$MYSQL_PASSWORD"
-MYSQL_AUTHSTR=" --host $MYSQL_HOST -u $MYSQL_USERNAME "
-RESULT=`mysql $MYSQL_AUTHSTR --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE'"`
-if [ "$RESULT" == "$MYSQL_DATABASE" ]; then
-    echo 'Database "'$MYSQL_DATABASE'" exists'
-else
-    echo "Database does not exist"
-    echo 'Creating database "'$MYSQL_DATABASE'" on host "'$MYSQL_HOST'"...'
-    mysql $MYSQL_AUTHSTR -e "CREATE DATABASE $MYSQL_DATABASE"
-    echo 'Database "'$MYSQL_DATABASE'" initialized successfully!'
-fi
 
 # Set mysql config parameters based on evironment variables
-for file in /DarkflameServer/build/masterconfig.ini; do
+for file in /DarkflameServer/build/sharedconfig.ini; do
     sed -i "s/mysql_host.*/mysql_host=$MYSQL_HOST/g" $file
     sed -i "s/mysql_database.*/mysql_database=$MYSQL_DATABASE/g" $file
     sed -i "s/mysql_username.*/mysql_username=$MYSQL_USERNAME/g" $file
     sed -i "s/mysql_password.*/mysql_password=$MYSQL_PASSWORD/g" $file
-    echo "Updated database settings in \"$file\" from environment vars"
+    sed -i "s/client_location.*/client_location=\/LegoFiles/g" $file
+    sed -i "s/external_ip.*/external_ip=$EXTERNAL_IP/g" $file
+    echo "Updated settings in \"$file\" from environment vars"
+done
+
+for file in /DarkflameServer/build/masterconfig.ini; do
+    sed -i "s/master_ip.*/master_ip=$MASTER_IP/g" $file
 done
 
 # Run the DLU Server
-./MasterServer
-
+./build/MasterServer
